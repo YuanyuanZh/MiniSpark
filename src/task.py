c9209ce13a6352765cb3f03a9d339c2a3758cbde
kaijiezhou <kaijiezhou913@gmail.com>
2015-11-29 21:07:10 -0800

    generate tasks

--- src/task.py
+++ src/task.py
@@ -1,15 +1,15 @@
 class Task(object):
-    def __init__(self, last_rdd, input_source, partition_id):
+    def __init__(self, last_rdd, input_source, task_id):
         """
-
         :param last_rdd:
         :param input_source:
-        :param partition_id: i_j (stage i, partition j)
+        :param task_id: i_j (stage i, partition j)
+        :return:
         """
         self.worker = None
         self.last_rdd = last_rdd
         self.input_source = input_source
-        self.partition_id = partition_id
+        self.task_id = task_id
         self.status = None
 
 
